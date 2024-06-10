0a85c33bf550a305c7511a9ddf69fe6256c9c488
RoogieB <raulsbergs@gmail.com>
2024-06-10 14:55:51 +0100

    expressions alert basic project

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/expressions_alert.js
@@ -0,0 +1,29 @@
+window.alert("alert"); //pop up alert in browser
+document.write("Hello World!");
+
+var a = "a string variable"; //assigning value to string
+document.write(a); //writing to the screen
+
+var b = "b string variable";
+window.alert(b);
+
+document.write(" where is my cat\'s toy \\ toys?"); 
+
+document.write(" Where is lucy ? " + " I can\'t find Amy either. ");
+
+var c = " Can\'t " + " see"
+document.write(c);
+
+var pets = "pets" , cat = "Lucy" , dog = "Dingo" , 
+chicken = "Riser" , mouse = "Jerry"; //creating multiple variables
+
+document.write(chicken); //writing on the screen using var chicken and displaying its name
+
+var num = 3 + 3; //expression
+
+var sent1 = "This is the beginning of the string "; //assigning value to string
+var sent2 = " and this is the end of the string"; //assigning value to string
+
+var sent3 = sent1 + sent2; //adding strings together
+
+document.write(sent3); //displaying concatenated string
